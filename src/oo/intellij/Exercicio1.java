2817fd30043601ae27c3a6549408b190496f7d3e
Julia <juliacris14@hotmail.com>
2021-12-17 15:52:36 -0300

    Foursys

--- src/oo/intellij/Exercicio1.java
+++ src/oo/intellij/Exercicio1.java
@@ -26,7 +26,9 @@ public class Exercicio1 {
 // exercicio18();
 //exercicio19();
 // exercicio20();
-        exercicio21();
+// exercicio21();
+// exercicio22();
+exercicio23();
     }
 
     public static void exercicio1() {
@@ -88,55 +90,59 @@ public class Exercicio1 {
 
         scan.close();
     }
-    public static void exercicio7(){
+
+    public static void exercicio7() {
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Digite sua Idade:");
         String idade = scan.nextLine();
 
         System.out.println("Digite  sua Altura:");
-        double altura =scan.nextDouble();
+        double altura = scan.nextDouble();
 
         System.out.println("Digite seu Peso:");
         double peso = scan.nextDouble();
 
     }
-    public static void exercicio8(){
+
+    public static void exercicio8() {
 
         Scanner scan = new Scanner(System.in);
 
-        System.out.print ("Digite seu Salário:");
+        System.out.print("Digite seu Salário:");
         double salario = scan.nextDouble();
         System.out.print("Digite seu Vale-Refeição:");
         double valerefeicao = scan.nextDouble();
-        System.out.printf("Seu Salário é %.0f e seu Vale-Refeição é %.0f",salario,valerefeicao);
+        System.out.printf("Seu Salário é %.0f e seu Vale-Refeição é %.0f", salario, valerefeicao);
 
     }
-    public static void exercicio9(){ ///Soma
+
+    public static void exercicio9() { ///Soma
 
         Scanner scan = new Scanner(System.in);
         System.out.println("Digite o valor de X:");
-        int x=scan.nextInt();
+        int x = scan.nextInt();
         System.out.println("Digite o valor de Y:");
-        int y=scan.nextInt();
-        int soma = x+y;
+        int y = scan.nextInt();
+        int soma = x + y;
 
-        System.out.printf("O resultado da soma entre a variável x e y é igual a:"+ soma);
+        System.out.printf("O resultado da soma entre a variável x e y é igual a:" + soma);
     }
-    public static void exercicio10(){ ///Subtração
+
+    public static void exercicio10() { ///Subtração
 
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Digite o valor de X:");
-        int x =scan.nextInt();
+        int x = scan.nextInt();
         System.out.println("Digite o valor de Y:");
-        int y= scan.nextInt();
-        int subtracao = x-y;
+        int y = scan.nextInt();
+        int subtracao = x - y;
 
-        System.out.printf("O resultado da substração entre a variável x e y é igual a:"+subtracao);
+        System.out.printf("O resultado da substração entre a variável x e y é igual a:" + subtracao);
     }
 
-    public static void exercicio11(){///Multiplicação
+    public static void exercicio11() {///Multiplicação
 
         Scanner scan = new Scanner(System.in);
 
@@ -144,12 +150,13 @@ public class Exercicio1 {
         int x = scan.nextInt();
         System.out.println("Digite o valor de y:");
         int y = scan.nextInt();
-        int multi = x*y;
+        int multi = x * y;
 
-        System.out.printf("O resultado da multiplicação entre a variavel x e y é igual a:"+multi);
+        System.out.printf("O resultado da multiplicação entre a variavel x e y é igual a:" + multi);
 
     }
-    public static void  exercicio12(){///Divisão
+
+    public static void exercicio12() {///Divisão
 
         Scanner scan = new Scanner(System.in);
 
@@ -157,37 +164,41 @@ public class Exercicio1 {
         int x = scan.nextInt();
         System.out.println("Digite o valor de y:");
         int y = scan.nextInt();
-        int div = x/y;
+        int div = x / y;
 
-        System.out.printf("O resultado da divisão entre a variável x e y é igual a:"+div);
+        System.out.printf("O resultado da divisão entre a variável x e y é igual a:" + div);
 
     }
-    public static void exercicio13(){///Exponenciação(Potência)
+
+    public static void exercicio13() {///Exponenciação(Potência)
 
         double x = 8;
         double y = 2;
-        double pon = Math.pow(x,y);
+        double pon = Math.pow(x, y);
 
-        System.out.printf("O resultado da exponencição entre a variavel x e y é igual a:"+ pon);
+        System.out.printf("O resultado da exponencição entre a variavel x e y é igual a:" + pon);
     }
-    public static void exercicio14(){///Modulo(Resto)
+
+    public static void exercicio14() {///Modulo(Resto)
         int x = 256;
         int y = 2;
-        double resto = Math.floorMod(x,y);
+        double resto = Math.floorMod(x, y);
 
-        System.out.printf("O resultado do Resto da divisão entre a variável x e y é igual a:"+resto);
+        System.out.printf("O resultado do Resto da divisão entre a variável x e y é igual a:" + resto);
 
     }
-    public static void exercicio15(){//Antecessor da Variável
+
+    public static void exercicio15() {//Antecessor da Variável
 
         Scanner scan = new Scanner(System.in);
         System.out.println("Digite o valor de x:");
         int x = scan.nextInt();
         int antecessor = x - 1;
 
-        System.out.printf("O resultado do Antecessor da variável x é igual a:"+antecessor);
+        System.out.printf("O resultado do Antecessor da variável x é igual a:" + antecessor);
     }
-    public static void exercicio16(){///Área do Retangulo
+
+    public static void exercicio16() {///Área do Retangulo
 
         Scanner scan = new Scanner(System.in);
 
@@ -195,11 +206,12 @@ public class Exercicio1 {
         double Base = scan.nextDouble();
         System.out.println("Digite o valor da Altura:");
         double Altura = scan.nextDouble();
-        double Resultado =Base*Altura;
+        double Resultado = Base * Altura;
 
-        System.out.println("A área Total do Retangulo é:"+ Resultado);
+        System.out.println("A área Total do Retangulo é:" + Resultado);
     }
-    public static void exercicio17(){
+
+    public static void exercicio17() {
 
         Scanner scan = new Scanner(System.in);
 
@@ -210,18 +222,19 @@ public class Exercicio1 {
         System.out.println("Digite quantos dias:");
         int dias = scan.nextInt();
 
-        int idadeAno = idade*365;
-        int idademes = meses*30;
+        int idadeAno = idade * 365;
+        int idademes = meses * 30;
         int idadedia = dias;
 
-        int DiasTotaisdeVida = idadeAno+idademes+idadedia;
+        int DiasTotaisdeVida = idadeAno + idademes + idadedia;
 
-        System.out.printf("Sua Idade total em dias vividos são:"+DiasTotaisdeVida);
+        System.out.printf("Sua Idade total em dias vividos são:" + DiasTotaisdeVida);
 
     }
-    public static void exercicio18(){
 
-}
+    public static void exercicio18() {
+
+    }
 
     public static void exercicio19() {
 
@@ -231,24 +244,26 @@ public class Exercicio1 {
             double x = scan.nextDouble();
             System.out.println("Digite o valor do percentual:");
             double y = scan.nextDouble();
-            double valorpercentual = (x*y)/100;
-            System.out.println("O valor total do percentual é:"+ valorpercentual);
+            double valorpercentual = (x * y) / 100;
+            System.out.println("O valor total do percentual é:" + valorpercentual);
             double salarionovo = x + valorpercentual;
-            System.out.printf("O novo salario:"+ salarionovo);
+            System.out.printf("O novo salario:" + salarionovo);
 
         }
 
-        }
-        public static void exercicio20(){
+    }
 
-            Scanner scan = new Scanner(System.in);
-            Locale.setDefault(Locale.US);
-            System.out.println("Digite o valor do Dolar:");
-            double dolar = scan.nextDouble();
-            double ConvReais = dolar * 6.61;
-            System.out.printf("O valor convertido de Dolar para Real:" +ConvReais);
-        }
-        public static void exercicio21(){
+    public static void exercicio20() {
+
+        Scanner scan = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+        System.out.println("Digite o valor do Dolar:");
+        double dolar = scan.nextDouble();
+        double ConvReais = dolar * 6.61;
+        System.out.printf("O valor convertido de Dolar para Real:" + ConvReais);
+    }
+
+    public static void exercicio21() {
 
         Scanner scan = new Scanner(System.in);
 
@@ -256,17 +271,32 @@ public class Exercicio1 {
         double custofabrica = scan.nextDouble();
         double impostos = 0.45;
         double Distribuidor = 0.28;
-        double custoimpostos = custofabrica*0.45;
-        double custodistribuidor = custofabrica* 0.28;
-        double custofinal = custoimpostos+custodistribuidor+custofabrica;
-        System.out.println("O Valor dos Impostos sobre Custo de Fábrica:"+custoimpostos);
-        System.out.println("0 Valor do Distribuidor sobre o Custo de Fábrica:"+ custodistribuidor);
-        System.out.println("O Custo Final do Carro novo é:"+custofinal);
-
-        }
+        double custoimpostos = custofabrica * 0.45;
+        double custodistribuidor = custofabrica * 0.28;
+        double custofinal = custoimpostos + custodistribuidor + custofabrica;
+        System.out.println("O Valor dos Impostos sobre Custo de Fábrica:" + custoimpostos);
+        System.out.println("0 Valor do Distribuidor sobre o Custo de Fábrica:" + custodistribuidor);
+        System.out.println("O Custo Final do Carro novo é:" + custofinal);
 
     }
 
+    public static void exercicio22() {
+
+
+    }
+
+
+    public static void exercicio23() {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite o grau Fahrenheit:");
+        float fahrenheit = scan.nextFloat();
+        float calculo = ((fahrenheit - 32)*5)/9;
+        System.out.println("O resultado da conversão de Fahrenheit para Celsius é:"+ calculo);
+
+    }
+}
 
 
 
